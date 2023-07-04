2a952e0cf8bac685a4843501722b712ed670a60a
jibundeyare <contact@jibundeyare.com>
2023-07-04 14:26:54 +0200

    Création du cours de SQL

---
+++ exercice-01.sql
@@ -0,0 +1,46 @@
+-- Exo 1
+-- Requêtes simples en lecture
+
+-- Code 1.1
+-- Voici la requête qui permet de lister tous les tags
+SELECT *
+FROM tag;
+
+-- Exo 1.1
+-- Écrivez la requête qui permet de lister tous les students
+
+-- Exo 1.2
+-- Écrivez la requête qui permet de lister tous les projects
+
+-- Exo 1.3
+-- Écrivez la requête qui permet de lister le student dont l'id est `2`
+
+-- Exo 1.4
+-- Écrivez la requête qui permet de lister les students dont l'id n'est pas `2`
+
+-- Exo 1.5
+-- Écrivez la requête qui permet de lister le project dont l'id est `3`
+
+-- Exo 1.6
+-- Écrivez la requête qui permet de lister les projects dont l'id n'est pas `3`
+
+-- Exo 1.7
+-- Écrivez la requête qui permet de lister les students dont l'email se termine par la chaîne de caractères `.com`
+
+-- Exo 1.8
+-- Écrivez la requête qui permet de lister les students dont la date de création est antérieure au 10 janvier 2021 inclus
+
+-- Exo 1.9
+-- Écrivez la requête qui permet de lister les projects dont la description ne contient pas la chaîne de caractères `Dolores`
+
+-- Exo 1.10
+-- Écrivez la requête qui permet de lister les projects dont la date de création est postérieure au 1er juillet 2021 inclus
+
+-- Exo 1.11
+-- Écrivez la requête qui permet de lister les students ayant un project
+-- Note : les students sans project ont une colonne `project_id` qui est nulle
+
+-- Exo 1.12
+-- Écrivez la requête qui permet de lister les students n'ayant pas de project
+-- Note : les students sans project ont une colonne `project_id` qui est nulle
+
---
+++ exercice-04.sql
@@ -0,0 +1,17 @@
+-- Exo 4
+-- Requêtes avec jointure « many to one » et « one to many »
+
+-- Exo 4.1
+-- Listez tous les students avec leurs projects
+-- Note : les students sans projets doivent aussi apparaître dans les résultats
+
+-- Exo 4.2
+-- Listez le student dont l'id est `2` avec son project
+
+-- Exo 4.3
+-- Listez tous les projects avec leurs students
+-- Note : les projets sans studets ne doivent pas apparaître dans les résultats
+
+-- Exo 4.4
+-- Listez le project dont l'id est `3` avec ses students
+
